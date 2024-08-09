d95159f9ac1e5a1bc81d2f0d528365ce2c5d51b6
appitronexpert <appitronexperts@gmail.com>
2024-08-09 14:03:48 +0530

    bug fixed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -356,9 +356,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "acf05a318ed28cb4e3cb760205c9c58e
 "icons/ic_launcher_bg_white.png": "1380d1ad78d2e2fd27a065e01427cb88",
 "icons/ninjapay_live.svg": "19a9284811379101cba4fa339a11c15c",
 "icons/ninjapay_live_link.png": "8f0dcf9e8ddf11e9f8a3ac0079317264",
-"index.html": "e979f510a486c819c36408d3b6407d40",
-"/": "e979f510a486c819c36408d3b6407d40",
-"main.dart.js": "1bab25e09be1cc4fab27837071b897c1",
+"index.html": "60ad70815d25130036dbf3615c6cbd35",
+"/": "60ad70815d25130036dbf3615c6cbd35",
+"main.dart.js": "375e796cd81848426ccfe16176f5a5f7",
 "manifest.json": "cdc2090520354438b06bd8dc0855a998",
 "ninjapay_live_link.png": "8f0dcf9e8ddf11e9f8a3ac0079317264",
 "version.json": "d4960b3b46d103862db9726c4d637f4e"};
