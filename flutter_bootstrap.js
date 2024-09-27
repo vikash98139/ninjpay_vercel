4dc14b953701d769670b88853fa69213a49fe26c
appitronexpert <appitronexperts@gmail.com>
2024-09-27 17:06:34 +0530

    mmmmmmm

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3441138534"
+    serviceWorkerVersion: "2852692731"
   }
 });
